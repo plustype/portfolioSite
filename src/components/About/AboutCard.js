6d7418726f57b140ac152d6da8181ebc38469219
plustype <zhaoyu.steven@gmail.com>
2024-01-24 10:25:12 -0800

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -11,13 +11,20 @@ function AboutCard() {
             Hi, I am <span className="purple">Steven Zhao.</span>
             <br />
             <br />
-            I am an experienced software engineer with a comprehensive background in full-stack development. I adept at working in cross-functional teams and collaborating with stakeholders to deliver projects on time and within budget.
+            I am an experienced software engineer with a comprehensive
+            background in full-stack development. I am adept at working in
+            cross-functional teams and collaborating with stakeholders to
+            deliver projects on time and within budget.
             <br />
             <br />
-            Beyond software, my experience extends to hardware system integration, product diagnostics and technical support.
+            Beyond software, my experience extends to hardware system
+            integration, product diagnostics and technical support.
             <br />
-            <br />
-            I am committed to best practices in development, emphasizing code quality, scalability, and security. Keen on evolving technologies, I continually upgrade my skill set. My agility in adapting to dynamic environments enables me to handle multiple priorities under tight deadlines effectively.
+            <br />I am committed to best practices in development, emphasizing
+            code quality, scalability, and security. Keen on evolving
+            technologies, I continually upgrade my skill set. My agility in
+            adapting to dynamic environments enables me to handle multiple
+            priorities under tight deadlines effectively.
           </p>
         </blockquote>
       </Card.Body>
